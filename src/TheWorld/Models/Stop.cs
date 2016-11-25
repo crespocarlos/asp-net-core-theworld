18e9da68a32ff8fe92b21b080a3f0845f01f871e
unknown <ccrespo@cit014456.cit>
2016-11-25 17:30:30 -0200

    bootstrap and ef core

---
+++ src/TheWorld/Models/Stop.cs
@@ -0,0 +1,13 @@
+﻿namespace TheWorld.Models
+{
+    public class Stop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public int Order { get; set; }
+        public double Arrival { get; set; }
+
+    }
+}
